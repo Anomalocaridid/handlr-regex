2795033c9056e3a8499add2eb5cba7c80dc591b5
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2022-04-21 18:57:50 -0400

    Tweak names in man page

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -19,13 +19,19 @@ fn dist() -> Result<()> {
 
 fn dist_manpage() -> Result<()> {
     let out_dir = dist_dir();
-
     let man = clap_mangen::Man::new(Cmd::command()).title("handlr");
-
     let mut buffer: Vec<u8> = Default::default();
 
+    // Render man page
     man.render(&mut buffer)?;
 
+    // Tweak man page and replace instances of handlr-regex with handlr except the title section
+    let re = regex::bytes::Regex::new(r"handlr\\-regex\\")
+        .expect("man page replacement regex invalid");
+
+    let buffer = re.replace_all(&buffer, r"handlr\".as_bytes());
+
+    // Write man page to file
     std::fs::create_dir_all(&out_dir)?;
     std::fs::write(&out_dir.join("handlr.1"), buffer)?;
 
