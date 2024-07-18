25387028900f61095709ac3a455ea5c4a3bb97df
Anomalocaridid <29845794+Anomalocaridid@users.noreply.github.com>
2024-07-17 21:52:31 -0400

    tests: add golden tests for `render_table`

--- handlr-regex/src/common/table.rs
+++ handlr-regex/src/common/table.rs
@@ -21,3 +21,44 @@ pub fn render_table<T: Tabled>(rows: &Vec<T>, terminal_output: bool) -> String {
     }
     .to_string()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::error::Result;
+    use itertools::Itertools;
+
+    #[derive(Tabled)]
+    struct TestRow<'a> {
+        col1: &'a str,
+        col2: &'a str,
+    }
+
+    // Arbitrary sample text
+    const LOREM_IPSUM: &str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+    // Helper function to create test data
+    fn rows(test_text: &str) -> Vec<TestRow<'_>> {
+        test_text
+            .split(' ')
+            .collect_vec()
+            .chunks_exact(2)
+            .map(|chunk| TestRow {
+                col1: chunk[0],
+                col2: chunk[1],
+            })
+            .collect_vec()
+    }
+
+    #[test]
+    fn terminal_output() -> Result<()> {
+        goldie::assert!(render_table(&rows(LOREM_IPSUM), true));
+        Ok(())
+    }
+
+    #[test]
+    fn piped_output() -> Result<()> {
+        goldie::assert!(render_table(&rows(LOREM_IPSUM), false));
+        Ok(())
+    }
+}
